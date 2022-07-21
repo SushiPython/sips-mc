f5f7884eb3ecc820dacb218b036a796ab761f2a1
Sushi <57518856+SushiPython@users.noreply.github.com>
2022-07-20 18:04:54 -0600

    no chunks

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
-	"runtime"
 	"strings"
-	"sync"
 	"github.com/alteamc/minequery/ping"
+	"github.com/korovkin/limiter"
 )
 
 func checkErr(err error) {
@@ -15,26 +14,7 @@ func checkErr(err error) {
 	}
 }
 
-func chunks(xs []string, chunkSize int) [][]string {
-    if len(xs) == 0 {
-        return nil
-    }
-    divided := make([][]string, (len(xs)+chunkSize-1)/chunkSize)
-    prev := 0
-    i := 0
-    till := len(xs) - chunkSize
-    for prev < till {
-        next := prev + chunkSize
-        divided[i] = xs[prev:next]
-        prev = next
-        i++
-    }
-    divided[i] = xs[prev:]
-    return divided
-}
-
-func checkServer(ip string, ipNum int, file *os.File, wg *sync.WaitGroup) {
-	defer wg.Done()
+func checkServer(ip string, ipNum int, file *os.File) {
 	resp, err := ping.Ping(ip, 25565)
 	if (err == nil) {
 		if motd, ok := resp.Description.(map[string]interface{}); ok {
@@ -45,38 +25,23 @@ func checkServer(ip string, ipNum int, file *os.File, wg *sync.WaitGroup) {
 
 func main() {
 	fmt.Println("beginning scan...")
+	limit := limiter.NewConcurrencyLimiter(10000) // set this to the number of concurrent requests you want to make
 	f, err := os.OpenFile("output.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	checkErr(err)
 	// read ips.txt and split by newline
-	content, err := os.ReadFile("ips.txt")
+	content, err := os.ReadFile("backup.txt")
 	
 	checkErr(err)
 	ips := strings.Split(string(content), "\n")
 
-	numCPU := runtime.NumCPU()
-	chunkSize := (len(ips) + numCPU - 1) / numCPU
 
-	ipsChunk := chunks(ips, chunkSize)
 
 	// iterate over ips
-	for index, chunk := range ipsChunk {
-		// create a new waitgroup
-		var wg sync.WaitGroup
-		// print chunk number starting
-		fmt.Println("chunk", index, "starting")
-		// add one to the waitgroup
-		fmt.Println("chunk length: ", len(chunk))
-		wg.Add(len(chunk))
-		// iterate over chunk
-		for ipNum, ip := range chunk {
-			// create a goroutine for each ip
-			go checkServer(ip, ipNum, f, &wg)
-		}
-		// wait for the goroutine to finish
-		wg.Wait()
-		// print chunk number ending
-		fmt.Println("chunk", index, "ending")
+	for index, chunk := range ips {
+		limit.Execute(func() {
+			checkServer(chunk, index, f)
+		})
 	}
-
+	defer limit.WaitAndClose()
 	fmt.Println("scan complete")
 }
